b4b61e9299724a1c73465f059f048e524028d73f
John Van Derton <101995974+johnvanderton@users.noreply.github.com>
2024-05-13 15:09:15 +0200

    Update index.ts
    Removed targeting test env (cserv)

--- index.ts
+++ index.ts
@@ -1,16 +1,10 @@
 import { Flysh, IOMessageMapper, InputMessage, PageRecords } from "flysh";
 
-// let IM = new InputMessage('.','/test.html');
-// IM.addFilterSelector("table tr td")
-//     .addField('prod','td','')
-//     .addField('brol1','td','')
-//     .addField('brol2','td','');
-
-let IM = new InputMessage('https://cserv.be','/soft/index_0.html');
-IM.addFilterSelector("table tr td")
-    .addField('field_1','td','')
-    .addField('field_2','td','')
-    .addField('field_3','td','');
+ let IM = new InputMessage('.','/test.html');
+ IM.addFilterSelector("table tr td")
+     .addField('prod','td','')
+     .addField('brol1','td','')
+     .addField('brol2','td','');
 
 console.log(IOMessageMapper.toJSON(IM));
 
