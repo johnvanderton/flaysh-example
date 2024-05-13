09a0d2416666ce484a56bdc68ed899f18309baf3
John Van Derton <101995974+johnvanderton@users.noreply.github.com>
2024-05-13 15:10:18 +0200

    Update index.js
    Removed targeting test env (cserv)

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 "use strict";
 exports.__esModule = true;
 var flysh_1 = require("flysh");
-// let IM = new InputMessage('.','/test.html');
-// IM.addFilterSelector("table tr td")
-//     .addField('prod','td','')
-//     .addField('brol1','td','')
-//     .addField('brol2','td','');
-var IM = new flysh_1.InputMessage('https://cserv.be', '/soft/index_0.html');
+let IM = new InputMessage('.','/test.html');
+
 IM.addFilterSelector("table tr td")
-    .addField('field_1', 'td', '')
-    .addField('field_2', 'td', '')
-    .addField('field_3', 'td', '');
+     .addField('prod','td','')
+     .addField('brol1','td','')
+     .addField('brol2','td','');
+
 console.log(flysh_1.IOMessageMapper.toJSON(IM));
+
 /**
  * todo create a cserv.be/test/page.html for distant tests
  */
